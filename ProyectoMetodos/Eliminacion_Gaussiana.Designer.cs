6219d2e087b4be561bee95877f837f8c694afbee
Vanessa <vlugoavalos@gmail.com>
2025-01-12 09:12:08 -0600

    escuaciones lineales sin button

--- ProyectoMetodos/Eliminacion_Gaussiana.Designer.cs
+++ ProyectoMetodos/Eliminacion_Gaussiana.Designer.cs
@@ -44,7 +44,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Engravers MT", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.label1.Location = new System.Drawing.Point(376, 21);
+            this.label1.Location = new System.Drawing.Point(375, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(513, 32);
             this.label1.TabIndex = 76;
@@ -54,7 +54,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(23, 76);
+            this.label6.Location = new System.Drawing.Point(22, 76);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(219, 21);
             this.label6.TabIndex = 83;
@@ -65,7 +65,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 111);
+            this.label2.Location = new System.Drawing.Point(22, 111);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(181, 21);
             this.label2.TabIndex = 84;
@@ -74,7 +74,7 @@
             // 
             // NumIncogtxt
             // 
-            this.NumIncogtxt.Location = new System.Drawing.Point(283, 71);
+            this.NumIncogtxt.Location = new System.Drawing.Point(282, 71);
             this.NumIncogtxt.Name = "NumIncogtxt";
             this.NumIncogtxt.Size = new System.Drawing.Size(100, 26);
             this.NumIncogtxt.TabIndex = 85;
@@ -84,7 +84,7 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(27, 142);
+            this.dataGridView1.Location = new System.Drawing.Point(26, 142);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
@@ -95,7 +95,7 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(618, 143);
+            this.dataGridView2.Location = new System.Drawing.Point(617, 143);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 62;
             this.dataGridView2.RowTemplate.Height = 28;
@@ -106,7 +106,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Lucida Sans", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(518, 527);
+            this.button1.Location = new System.Drawing.Point(517, 527);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(176, 88);
             this.button1.TabIndex = 88;
--- ProyectoMetodos/Sist_Ec_Lineales.cs
+++ ProyectoMetodos/Sist_Ec_Lineales.cs
@@ -8,19 +8,20 @@ namespace ProyectoMetodos
 {
     internal class Sist_Ec_Lineales
     {
-        
+
         public bool eliminacion_gaussiana(float[,] matriz, int n)
         {
             int i, j, r;
+            float multiplicador;
 
             for (i = 0; i < n - 1; i++)
             {
                 for (r = i + 1; r < n; r++)
                 {
-                    float multiplicador = -matriz[r, i] / matriz[i, i];
+                    multiplicador = -matriz[r, i] / matriz[i, i];
                     matriz[r, i] = 0;
-            
-                    for (j = i + 1; j <= n ; j++)
+
+                    for (j = i + 1; j <= n; j++)
                     {
                         matriz[r, j] = multiplicador * matriz[i, j] + matriz[r, j];
                     }
@@ -32,7 +33,7 @@ namespace ProyectoMetodos
         public float[] SustitucionHaciaAtras(float[,] matriz, int n)
         {
             float[] soluciones = new float[n];
-            int i; 
+            int i;
 
             for (i = n - 1; i >= 0; i--)
             {
@@ -45,6 +46,131 @@ namespace ProyectoMetodos
             }
 
             return soluciones;
+        }
+
+        public bool PivoteoMaximoColumna(float[,] matriz, int n)
+        {
+            int i, j, r;
+            int RengInterec;
+            float aux;
+            float multiplicador;
+
+            for (i = 0; i < n; i++)
+            {
+                // Encontrar el valor máximo absoluto en la columna i
+                RengInterec = -1;
+                aux = Math.Abs(matriz[i, i]);
+                for (r = i + 1; r < n; r++)
+                {
+                    if (Math.Abs(matriz[r, i]) > aux)
+                    {
+                        RengInterec = r;
+                        aux = Math.Abs(matriz[r, i]);
+                    }
+                }
+
+                // Intercambiar filas si se encuentra un nuevo pivote
+                if (RengInterec != -1)
+                {
+                    for (j = 1; j <= n; j++) // j comienza en 0
+                    {
+                        aux = matriz[i, j];
+                        matriz[i, j] = matriz[RengInterec, j];
+                        matriz[RengInterec, j] = aux;
+                    }
+                }
+
+                // Verificar si el pivote es cero (división imposible)
+                if (matriz[i, i] == 0)
+                {
+                    continue;
+                }
+
+                // Realizar eliminación hacia adelante
+                for (r = i + 1; r < n; r++)
+                {
+                    multiplicador = -matriz[r, i] / matriz[i, i];
+                    matriz[r, i] = 0; // Eliminar este elemento
+
+                    for (j = i + 1; j <= n; j++) // j comienza en i + 1
+                    {
+                        matriz[r, j] = multiplicador * matriz[i, j] + matriz[r, j];
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool PivoteoParcialEscalado(float[,] matriz, int n)
+        {
+            int[] permutacion = new int[n];
+            float[] escalas = new float[n];
+
+            // Inicializar las permutaciones y las escalas
+            for (int i = 0; i < n; i++)
+            {
+                permutacion[i] = i;
+                escalas[i] = 0;
+
+                // Calcular la escala como el valor máximo absoluto en cada fila
+                for (int j = 0; j < n; j++)
+                {
+                    escalas[i] = Math.Max(escalas[i], Math.Abs(matriz[i, j]));
+                }
+
+                // Verificar si la fila es nula (escala cero)
+                if (escalas[i] == 0)
+                {
+                    return false; // El sistema no tiene solución
+                }
+            }
+
+            // Iterar sobre cada columna para triangular la matriz
+            for (int k = 0; k < n; k++)
+            {
+                // Encontrar el pivote usando el criterio de escalado
+                int pivoteFila = k;
+                float maxRatio = 0;
+
+                for (int i = k; i < n; i++)
+                {
+                    float ratio = Math.Abs(matriz[permutacion[i], k]) / escalas[permutacion[i]];
+                    if (ratio > maxRatio)
+                    {
+                        maxRatio = ratio;
+                        pivoteFila = i;
+                    }
+                }
+
+                // Intercambiar filas en la permutación
+                if (pivoteFila != k)
+                {
+                    int temp = permutacion[k];
+                    permutacion[k] = permutacion[pivoteFila];
+                    permutacion[pivoteFila] = temp;
+                }
+
+                // Verificar si el pivote es cero (matriz singular)
+                if (matriz[permutacion[k], k] == 0)
+                {
+                    return false; // No se puede triangular
+                }
+
+                // Eliminar hacia adelante para triangular la matriz
+                for (int i = k + 1; i < n; i++)
+                {
+                    float factor = -matriz[permutacion[i], k] / matriz[permutacion[k], k];
+                    matriz[permutacion[i], k] = 0; // Eliminar el elemento debajo del pivote
+
+                    for (int j = k + 1; j < n; j++)
+                    {
+                        matriz[permutacion[i], j] += factor * matriz[permutacion[k], j];
+                    }
+                }
+            }
+
+            // Si llegamos aquí, el pivoteo fue exitoso
+            return true;
         }
     }
 }
